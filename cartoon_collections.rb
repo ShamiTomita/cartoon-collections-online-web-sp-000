9077bc1949d85850bb49fd051f712550a497caf0
Shami Tomita <shamitomita@gmail.com>
2021-01-08 17:00:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,8 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect(cheese_types[0]||[1]||[2]) do |x|
-    if x == cheese_types[i]
-      return cheese_types[i]
+    if x == cheese_types[0]||[1]||[2]
+      return cheese_types[0] || [1] || [2]
     end 
   end 
 end
